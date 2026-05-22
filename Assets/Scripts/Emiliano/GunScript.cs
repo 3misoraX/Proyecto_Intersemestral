16923aa56839ec8a1d88127950aca3e343b91896
3misoraX <mano.alvagon@gmail.com>
2026-05-22 09:39:35 -0600

    Update GunScript.cs

--- Assets/Scripts/Emiliano/GunScript.cs
+++ Assets/Scripts/Emiliano/GunScript.cs
@@ -4,21 +4,37 @@ using UnityEngine.InputSystem;
 public class GunScript : MonoBehaviour
 {
     [SerializeField] private InputActionReference shootAction;
-    [SerializeField] private InputActionReference specialAction;
+    //uncomment this function if you end up adding the UseSpecial() function
+    //[SerializeField] private InputActionReference specialAction;
     [SerializeField] private InputActionReference switchAction;
     [SerializeField] private Transform shootPoint;
+    public GameObject activeAbility;
     public GameObject bulletPrefab;
     public float bulletForce;
     public float bulletDuration;
-    
-    // Update is called once per frame
+
+
     void Update()
     {
+        //prefab shooting
         if (shootAction.action.triggered)
         {
             GameObject projectile = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
             projectile.GetComponent<Rigidbody>().AddForce(transform.forward * bulletForce, ForceMode.VelocityChange);
             Destroy(projectile, bulletDuration);
         }
+        //could add raycast shooting with certain weapons
     }
+
+    void ChangeWeapons()
+    {
+        //This function should change the active object, which should be a scriptable object
+    }
+
+    /*  not sure if this will be added
+    void UseSpecial()
+    {
+        This function should use the special marked in a scriptable object
+    }
+    */
 }
