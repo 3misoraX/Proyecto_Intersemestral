2057c78b3aee546e2e4ddc0f9d75bdc5503f9c55
RodrigoGdeQuevedo <rodrigo.garcia@uniat.edu.mx>
2026-07-17 14:04:18 -0600

    Cambios en MainMenu, GameOver y PauseMenu

--- Assets/Scripts/Rodri_Scripts/GameOverController.cs
+++ Assets/Scripts/Rodri_Scripts/GameOverController.cs
@@ -5,10 +5,24 @@ public class GameOverController : MonoBehaviour
 {
     [Header("Scene Names")]
     [SerializeField] private string mainMenuSceneName = "MainMenu";
+    [SerializeField] private string gameplaySceneName = "DemoGame";
 
-    // Llamar desde el botón "Main Menu"
     public void ReturnToMainMenu()
     {
         SceneManager.LoadScene(mainMenuSceneName);
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(gameplaySceneName);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
